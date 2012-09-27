7e4efa405b27e419897f1b50b74051fb324deefc
Etienne Perot <etienne@perot.me>
2012-09-27 13:49:00 -0400

    Use link name as title in included markdown files

--- res/process-include.py
+++ res/process-include.py
@@ -1,6 +1,16 @@
 import os, re, markdown, filefilter
 
 rInclude = re.compile(r'<include([^<>]*)>((?:(?!</include>).)+)</include>', re.IGNORECASE)
+rLinkMatch = re.compile(r'^\[([^]\r\n]+)\]:[ \t]*(\S+)(?:[ \t]+([^\r\n]+))?', re.MULTILINE)
+
+def handleLink(match):
+	url = match.group(2)
+	if '/' not in url:
+		url = (self.resourceUrl + '/' + url).replace('//', '/')
+	title = match.group(3)
+	if not title:
+		title = '"' + match.group(1) + '"'
+	return '[' + match.group(1) + ']: ' + url + ' ' + title
 
 def processInclude(f, m):
 	toInclude = filefilter.findFile(f, m.group(2).strip())
@@ -11,6 +21,7 @@ def processInclude(f, m):
 	content = handle.read(-1)
 	handle.close()
 	if isMarkdown:
+		content = rLinkMatch.sub(handleLink, content)
 		content = markdown.Markdown(
 			extensions=['codehilite', 'meta', 'sane_lists'],
 			extensions_configs={},
