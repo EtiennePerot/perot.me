3e1e0e055adb1cf7cb44c3317fc741271d388895
Etienne Perot <etienne@perot.me>
2013-01-12 21:01:02 -0500

    Skip PHP tags when filtering HTML

--- res/filefilter.py
+++ res/filefilter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, sys
+import os, sys, re
 
 def findFile(contextFile, fileToFind):
 	contextDir = os.path.abspath(contextFile)
@@ -20,6 +20,20 @@ def process(f, content):
 		content = fi(f, content)
 	return content
 
+_isJustPhp = re.compile(r'^<\?php\s(?:(?!\?>).)*(?:\?>)?\s*$', re.IGNORECASE | re.DOTALL)
+phpTag = re.compile(r'<\?php\s.*?(?:\?>|$)', re.IGNORECASE | re.DOTALL)
+phpInjectTag = re.compile(r'<php id="(\d+)" />')
+def isJustPhp(content):
+	return _isJustPhp.match(content)
+def stripPhp(content):
+	tags = []
+	def insertTag(m):
+		tags.append(m.group(0))
+		return '<php id="' + str(len(tags) - 1) + '" />'
+	return phpTag.sub(insertTag, content), tags
+def restorePhp(content, tags):
+	return phpInjectTag.sub(lambda m : tags[int(m.group(1))], content)
+
 if __name__ == '__main__':
 	args = sys.argv[1:]
 	for a in sys.argv[1:]:
@@ -32,11 +46,14 @@ if __name__ == '__main__':
 		print('Specify at least one file to parse.', file=sys.stderr)
 		sys.exit(1)
 	for f in files:
-		if '.include' in f:
+		if '.include' in f or '.template' in f:
 			continue
 		handle = open(f, 'r', encoding='utf8')
 		content = handle.read(-1)
 		handle.close()
+		if isJustPhp(content):
+			continue
+		content, phpTags = stripPhp(content)
 		originalContent = content
 		newContent = process(f, content)
 		while newContent != content:
@@ -44,5 +61,6 @@ if __name__ == '__main__':
 			newContent = process(f, content)
 		if newContent != originalContent:
 			handle2 = open(f, 'w', encoding='utf8')
+			newContent = restorePhp(newContent, phpTags)
 			handle2.write(newContent)
 			handle2.close()
